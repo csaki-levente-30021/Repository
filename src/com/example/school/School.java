9460e351857718806d4679ecb39ed35a6c1c5333
leventecsaki <leventecsaki90@yahoo.com>
2021-03-03 12:23:36 +0200

    Second commit

--- src/com/example/school/School.java
+++ src/com/example/school/School.java
@@ -1,7 +1,36 @@
 package com.example.school;
 
+import javax.swing.plaf.basic.BasicTreeUI;
+
 public class School {
+    String name;
+    int age;
+    String schoolName;
+    String city;
+
+    public School(String fn, int ag, String schname, String cityes) {
+        name = fn;
+        age = ag;
+        schoolName = schname;
+        city = cityes;
+    }
+
+    public void displaySchool() {
+        System.out.println("Student name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("School name: " + schoolName);
+        System.out.println("City: " + city);
+        System.out.println("\n");
+    }
+
     public static void main(String[] args) {
-        System.out.println("It is my school");
+        School s1 = new School("John", 18, "Utcn", "Cluj");
+        s1.displaySchool();
+
+        School s2 = new School("Fred", 22, "Ubb", "Oradea");
+        s2.displaySchool();
+
+        School s3 = new School("Tom", 21, "Utb", "Brasov");
+        s3.displaySchool();
     }
 }
